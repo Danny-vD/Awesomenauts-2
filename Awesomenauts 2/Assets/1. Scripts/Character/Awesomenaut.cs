ed903bda726a64ec9e0e782123f70a947733c88a
Danny van Duist <dvduist@hotmail.nl>
2020-02-24 13:31:57 +0100

    Very bare-bones awesomenaut class. Updated .editorconfig.

---
+++ Awesomenauts 2/Assets/1. Scripts/Character/Awesomenaut.cs
@@ -0,0 +1,13 @@
+﻿using VDFramework;
+
+namespace Character
+{
+    public abstract class Awesomenaut : BetterMonoBehaviour
+    {
+		public uint MaxHealth { get; protected set; }
+		
+		public uint Health { get; protected set; }
+		
+		public string Name { get; protected set; }
+    }
+}
