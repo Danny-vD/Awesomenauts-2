8f7cd57a73a9fa9690fd08566560239f8e503f67
Danny van Duist <dvduist@hotmail.nl>
2020-03-16 12:18:14 +0100

    Switched the VDFramework.dll for the actual source code.

---
+++ Awesomenauts 2/Assets/zExternalCode/VDFramework/VDUnityFramework/UnityExtensions/GameObjectExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection.Emit;
+using UnityEngine;
+
+namespace VDFramework.UnityExtensions
+{
+	public static class GameObjectExtensions
+	{
+		/// <summary>
+		/// Ensures that the specified <see cref="TComponent"/> is on this <see cref="GameObject"/>.
+		/// </summary>
+		public static TComponent EnsureComponent<TComponent>(this GameObject gameObject) where TComponent : Component
+		{
+			return gameObject.TryGetComponent(out TComponent component)
+				? component
+				: gameObject.AddComponent<TComponent>();
+		}
+
+		public static TComponent EnsureComponent<TComponent>(this Component monoBehaviour) where TComponent : Component
+		{
+			return monoBehaviour.gameObject.EnsureComponent<TComponent>();
+		}
+	}
+}
