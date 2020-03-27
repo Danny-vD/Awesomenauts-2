ea598df5411a146db9e4d19e0ce0b711261f066a
Tim Akermann <ti.akermann@gmail.com>
2020-03-27 03:37:28 +0100

    Added Card Designs Added Card Socket Design Some Networking Fixes.

--- Awesomenauts 2/Assets/1. Scripts/Networking/CardNetworkManager.cs
+++ Awesomenauts 2/Assets/1. Scripts/Networking/CardNetworkManager.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Threading;
 using Mirror;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
 
 public class CardNetworkManager : NetworkManager
 {
@@ -27,6 +29,8 @@ public class CardNetworkManager : NetworkManager
 	private bool GameStarted => BoardLogic.Logic != null && BoardLogic.Logic.GameStarted;
 	public bool IsHost { get; private set; }
 	public bool IsServer { get; private set; }
+	private bool IsStarted;
+	private bool IsStopping;
 	public MapEntry[] AvailableMaps;
 	public CardEntry[] CardEntries;
 	public int[] CardsInDeck = new[] { 0, 1, 0, 1, 0, 1, 0, 1, 0 };
@@ -49,6 +53,7 @@ public class CardNetworkManager : NetworkManager
 		BoardLogic.Logic = null;
 		IsServer = false;
 		IsHost = false;
+		IsStarted = false;
 	}
 
 	public void SetMapToLoad(int id)
@@ -66,13 +71,14 @@ public class CardNetworkManager : NetworkManager
 	public override void OnServerDisconnect(NetworkConnection conn)
 	{
 		Debug.Log("Client Disconnected. Remaining: " + numPlayers);
-		if (numPlayers == 1) StopServer();
+		if (numPlayers == 1 && !IsStopping) StopServer();
 		base.OnServerDisconnect(conn);
 	}
 
 	public override void OnStopServer()
 	{
-
+		IsStopping = true;
+		IsStarted = false;
 		Debug.Log("Server Stopped.");
 		CleanUp();
 		base.OnStopServer();
@@ -87,6 +93,8 @@ public class CardNetworkManager : NetworkManager
 
 	public override void OnStartServer()
 	{
+		IsStopping = false;
+		IsStarted = true;
 		Debug.Log("Is Server");
 		IsServer = true;
 		base.OnStartServer();
@@ -110,7 +118,19 @@ public class CardNetworkManager : NetworkManager
 
 	private void Update()
 	{
-		if (!IsHost) return;
+		if (!IsStarted && SystemInfo.graphicsDeviceType == GraphicsDeviceType.Null) //not started and Headless
+		{
+			if (SceneManager.GetActiveScene().name == "MenuScene")
+			{
+				Debug.Log("Starting Server...");
+				StartServer();
+			}
+			return;
+		}
+
+
+		if (!IsServer) return;
+
 		if (CardPlayer.ServerPlayers.Count == 2 && CardPlayer.AllPlayersReady && !BoardLogic.Logic.GameStarted)
 		{
 			//Thread.Sleep(1000); //Hack: Wait for client to create the player after connection established.
