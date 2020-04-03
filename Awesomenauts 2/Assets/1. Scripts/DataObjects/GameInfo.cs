232a648130ff5ceb444df9791828103b4e1f2f0a
Tim Akermann <ti.akermann@gmail.com>
2020-04-03 11:03:24 +0200

    Added FPS Target under Game.TargetFPS:60

--- Awesomenauts 2/Assets/1. Scripts/DataObjects/GameInfo.cs
+++ Awesomenauts 2/Assets/1. Scripts/DataObjects/GameInfo.cs
@@ -7,12 +7,14 @@ namespace Networking {
 		public HeadlessServerInfo HeadlessInfo;
 		public GameNetworkInfo Network;
 		public DebugInfo DebugInfo;
+		public int TargetFPS;
 
 		public GameInfo()
 		{
 			HeadlessInfo = new HeadlessServerInfo();
 			Network = new GameNetworkInfo();
 			DebugInfo = new DebugInfo();
+			TargetFPS = 60;
 		}
 	}
 }
