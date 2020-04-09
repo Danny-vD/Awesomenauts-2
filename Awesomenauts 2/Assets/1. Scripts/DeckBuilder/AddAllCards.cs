65b0831c24c54388480fc0cfa356ac1465b8fe95
Danny van Duist <dvduist@hotmail.nl>
2020-04-09 11:21:06 +0200

    Made a small start.

---
+++ Awesomenauts 2/Assets/1. Scripts/DeckBuilder/AddAllCards.cs
@@ -0,0 +1,46 @@
+﻿using Networking;
+using UnityEngine;
+using VDFramework;
+
+namespace DeckBuilder
+{
+	/// TODO: make my own cardManager
+	/// which has a list of "Cards"
+	/// cardsEntries can have either a prefab with a card class or some sort
+	/// or just an image etc.
+	
+	public class AddAllCards : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private GameObject[] cardParents = new GameObject[0];
+
+		private void Start()
+		{
+			foreach (GameObject cardParent in cardParents)
+			{
+				AddCardsAsChild(cardParent);
+			}
+		}
+
+		private static void AddCardsAsChild(GameObject parent)
+		{
+			Transform parentTransform = parent.transform;
+
+			foreach (CardEntry entry in CardNetworkManager.Instance.CardEntries)
+			{
+				InstantiateCardEntry(entry, parentTransform);
+			}
+		}
+
+		private static void InstantiateCardEntry(CardEntry entry, Transform parent)
+		{
+			if (entry.Prefab == null)
+			{
+				return;
+			}
+				
+				
+			Instantiate(entry.Prefab, parent);
+		}
+	}
+}
