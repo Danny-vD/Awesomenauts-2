2876bc9f13a32a8634a38363e74345b124c96d25
Danny van Duist <dvduist@hotmail.nl>
2020-06-26 16:47:47 +0200

    Add exitButton script to manage the sprites.

---
+++ Awesomenauts 2/Assets/1. Scripts/UI/DeckBuilder/ExitButton.cs
@@ -0,0 +1,120 @@
+﻿using Events.Deckbuilder;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using VDFramework;
+using VDFramework.EventSystem;
+using VDFramework.UnityExtensions;
+
+namespace UI.DeckBuilder
+{
+	[RequireComponent(typeof(Image))]
+	public class ExitButton : BetterMonoBehaviour
+	{
+		[SerializeField]
+		private Sprite NormalSprite = null;
+		
+		[SerializeField]
+		private Sprite HoverSprite = null;
+		
+		[SerializeField]
+		private Sprite UnavailableSprite = null;
+
+		private Image image = null;
+		
+		private bool canClick = true;
+		
+		private Sprite Sprite
+		{
+			get => image.sprite;
+			set => image.sprite = value;
+		}
+
+		private void Awake()
+		{
+			image = GetComponent<Image>();
+		}
+
+		private void OnEnable()
+		{
+			AddListeners();
+			AddEventTriggers();
+		}
+
+		private void OnDisable()
+		{
+			RemoveListeners();
+		}
+		
+		private void AddListeners()
+		{
+			EventManager.Instance.AddListener<ValidDeckEvent>(OnCheckingDeck);
+		}
+		
+		private void RemoveListeners()
+		{
+			if (!EventManager.IsInitialized)
+			{
+				return;
+			}
+    
+			EventManager.Instance.RemoveListener<ValidDeckEvent>(OnCheckingDeck);
+		}
+
+		private void OnCheckingDeck(ValidDeckEvent validDeckEvent)
+		{
+			canClick = validDeckEvent.IsValid;
+
+			Sprite = canClick ? NormalSprite : UnavailableSprite;
+		}
+
+		private void OnPointerEnter()
+		{
+			if (canClick)
+			{
+				Sprite = HoverSprite;
+			}
+		}
+
+		private void OnPointerClick()
+		{
+			if (canClick && Input.GetMouseButtonDown(0))
+			{
+				GetComponentInParent<ButtonFunctionality>().LoadScene(1);
+			}
+		}
+
+		private void OnPointerExit()
+		{
+			if (canClick)
+			{
+				Sprite = NormalSprite;
+			}
+		}
+
+		private void AddEventTriggers()
+		{
+			EventTrigger.Entry pointerEnterEntry =
+				new EventTrigger.Entry
+					{eventID = EventTriggerType.PointerEnter};
+			pointerEnterEntry.callback.AddListener((data) => { OnPointerEnter(); });
+
+			EventTrigger.Entry pointerClickEntry =
+				new EventTrigger.Entry
+					{eventID = EventTriggerType.PointerClick};
+			pointerEnterEntry.callback.AddListener((data) => { OnPointerClick(); });
+
+			EventTrigger.Entry pointerExitEntry =
+				new EventTrigger.Entry
+					{eventID = EventTriggerType.PointerExit};
+			pointerExitEntry.callback.AddListener((data) => { OnPointerExit(); });
+
+			EventTrigger trigger =
+				gameObject.EnsureComponent<EventTrigger>();
+
+			trigger.triggers.Add(pointerEnterEntry);
+			trigger.triggers.Add(pointerClickEntry);
+			trigger.triggers.Add(pointerExitEntry);
+		}
+	}
+}
