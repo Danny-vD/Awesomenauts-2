05cd37f9515fa13eaf802c40f9b02d6f412a150e
Danny van Duist <dvduist@hotmail.nl>
2020-07-07 23:11:59 +0200

    Created a simple intro animation on level load.

---
+++ Awesomenauts 2/Assets/1. Scripts/Utility/GameobjectEnabler.cs
@@ -0,0 +1,27 @@
+﻿using VDFramework.VDUnityFramework.BaseClasses;
+
+namespace AwsomenautsCardGame.Utility
+{
+	public class GameobjectEnabler : BetterMonoBehaviour
+	{
+		public void SetActiveState(bool isActive)
+		{
+			gameObject.SetActive(isActive);
+		}
+
+		public void SetActive()
+		{
+			SetActiveState(true);
+		}
+
+		public void SetDisabled()
+		{
+			SetActiveState(false);
+		}
+
+		public void ToggleActive()
+		{
+			SetActiveState(!gameObject.activeSelf);
+		}
+	}
+}
